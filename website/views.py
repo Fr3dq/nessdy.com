f680f9062398831d6fbf8e8a69f8764683d694d8
fr3dq <fryderyk.otolski@gmail.com>
2025-04-14 17:29:08 +0200

    Upload folder changed. Error fixed

--- website/views.py
+++ website/views.py
@@ -9,8 +9,8 @@ from werkzeug.utils import secure_filename
 from datetime import datetime
 
 # Uploading images
-UPLOAD_FOLDER = r'static\uploads'
-UPLOAD_FOLDER_2 = r'C:\Users\Fryderyk\Nessdy.com\website\static\uploads'
+basedir = os.path.abspath(os.path.dirname(__file__))
+UPLOAD_FOLDER_2 = os.path.join(basedir, 'static', 'uploads')
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 
 def allowed_file(filename):
